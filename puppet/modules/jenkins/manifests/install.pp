a530cc59344d502cd8863543b4c6026a6effcc0c
saxsir <saxsir.256+github@gmail.com>
2015-05-12 18:06:38 +0900

    [fix] jenkinsのインストールコマンドをrootで実行しても動くように修正
    - sudoが使えないとエラーになってたので修正

--- puppet/modules/jenkins/manifests/install.pp
+++ puppet/modules/jenkins/manifests/install.pp
@@ -1,11 +1,11 @@
 class jenkins::install{
     exec { "add-jenkins-repo":
-        command => "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo",
+        command => "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo",
         path => ["/bin", "/usr/bin"],
     }
 
     exec { "install-jenkins-key":
-        command => "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
+        command => "rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key",
         path => ["/bin", "/usr/bin"],
     }
 
